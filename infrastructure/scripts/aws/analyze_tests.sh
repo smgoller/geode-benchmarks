29f8cdee423f0e527d8d2da1aece74797a8e792c
Sean Goller <sgoller@pivotal.io>
2019-01-14 08:43:36 -0800

    Automatically run test analysis when benchmarking.
    * Add script to analyze test results
    * Modify comparison benchmark script to run analyze script.
    
    Authored-by: Sean Goller <sgoller@pivotal.io>

---
+++ infrastructure/scripts/aws/analyze_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+OUTPUT_DIR=${1}
+
+TOP_DIR=$(git rev-parse --show-toplevel)
+
+BASELINE_DIR="${OUTPUT_DIR}/baseline"
+BRANCH_DIR="${OUTPUT_DIR}/branch"
+BASELINE_BENCHMARKS="$(ls -td ${BASELINE_DIR}/benchmarks_* | tail -1)"
+BRANCH_BENCHMARKS="$(ls -td ${BRANCH_DIR}/benchmarks_* | tail -1)"
+pushd ${TOP_DIR}
+./gradlew analyzeRun --args "${BASELINE_BENCHMARKS} ${BRANCH_BENCHMARKS}"
+popd

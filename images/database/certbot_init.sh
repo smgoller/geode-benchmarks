050422b82bcf3a9fd3abd6e19d1e508f84ab7862
Sean Goller <sgoller@pivotal.io>
2019-02-13 10:11:43 -0800

    Fix up init script.
    Authored-by: Sean Goller <sgoller@pivotal.io>

--- images/database/certbot_init.sh
+++ images/database/certbot_init.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -x
-CERTBOT_HOSTNAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/certbot-hostname -H
-"Metadata-Flavor: Google")
+CERTBOT_HOSTNAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/certbot-hostname -H "Metadata-Flavor: Google")
 echo "Running certbot..."
 echo "HOSTNAME is ${CERTBOT_HOSTNAME}"
-certbot certonly -n --dns-google --agree-tos --email sgoller@pivotal.io -d ${CERTBOT_HOSTNAME}                    
+pip3 install -U certbot-dns-google
+pip3 install -U certbot
+certbot certonly -n --dns-google --dns-google-propagation-seconds=300 --agree-tos --email sgoller@pivotal.io -d ${CERTBOT_HOSTNAME}
 
